f91fb24fc0cb9190c508a17c4bb6ed63d188d143
Brandon Aguirre <brag4918@colorado.edu>
2018-11-07 21:34:16 -0700

    update clean.sh to leave .gitkeep files

--- data_procuration/clean.sh
+++ data_procuration/clean.sh
@@ -2,8 +2,9 @@
 
 # this script will remove any data downloaded on your system
 
-rm ./data/img/*
-rm ./data/meta/*
+rm ./data/img/*.jpg
+rm ./data/meta/*.json
+rm ./data/img_mod/*.jpg
 echo "" > ./conf/current_ids
 echo "" > ./conf/ids
 echo "" > ./conf/list

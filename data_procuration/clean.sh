1bd9b41ec51ad555c224e9e8830f4807dbc26590
Brandon Aguirre <brag4918@colorado.edu>
2018-11-29 22:38:51 -0700

    update clean.sh to include new dirs

--- data_procuration/clean.sh
+++ data_procuration/clean.sh
@@ -5,6 +5,8 @@
 rm ./data/img/*.jpg
 rm ./data/meta/*.json
 rm ./data/img_mod/*.jpg
+rm ./data/img_crop/*.png
+rm ./data/meta_mask/*.png
 echo "" > ./conf/current_ids
 echo "" > ./conf/ids
 echo "" > ./conf/list
